4d3e825c23877e6f031cec34aa2323b52eab7604
Abhitesh Nandipati <116545078+n-abhitesh@users.noreply.github.com>
2024-04-24 08:42:06 +0530

    Update

---
+++ character_lexenes.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_LEN 100
+
+void tokenize(char input[]) {
+    char *token;
+    token = strtok(input, " ,.?!"); // split by space, comma, period, question mark, or exclamation mark
+
+    while(token != NULL) {
+        printf("%s\n", token);
+        token = strtok(NULL, " ,.?!");
+    }
+}
+
+int main() {
+    char input[MAX_LEN];
+    printf("Enter a sentence: ");
+    fgets(input, MAX_LEN, stdin);
+
+    // Remove newline character if present
+    if(input[strlen(input) - 1] == '\n') {
+        input[strlen(input) - 1] = '\0';
+    }
+
+    tokenize(input);
+
+    return 0;
+}
+
